c3acc83d40d7198aa7800aca86f20e9ff27e1872
Lucky <lk5999950@gmail.com>
2023-06-08 17:39:29 +0530

    First Commit

--- Recursion/Decreasing_number.c
+++ Recursion/Decreasing_number.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
-int decreasing(int a){
-    
+void decreasing(int n){
+    printf("%d\n", n);
+    if ( n==1 ){
+        return;
+    }
+    decreasing(n-1);
+    return;
 }
 int main(){
+    int n;
+    printf("Enter number n : ");
+    scanf("%d", &n);
+    decreasing(n);
     return 0;
 }
