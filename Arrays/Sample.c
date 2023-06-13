224dd8a21467919ce9f9d3efce55f61b4050831f
Lucky <lk5999950@gmail.com>
2023-06-13 20:52:15 +0530

    First Commit

--- Arrays/Sample.c
+++ Arrays/Sample.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main() {
 
-    // int arr[5];  // 5 dabbe ban agye & array only declare huya hai.
-    int arr[5] = { 2,4,7,1,9 }; // Declaration & initialisation both.
-    arr[3] = 100;  // index 3 ka element 1 se ab 100 ban gya.
+    // int arr[5];     // 5 dabbe ban agye & array only declare huya hai.
+    int arr[5] = { 2,4,7,1,9 };       // Declaration & initialisation both.
+    arr[3] = 100;      // index 3 ka element 1 se ab 100 ban gya.
     float b [3] = {1.4, 8.25, 10.7};
     char ch [4] = {'A','c','@','%'};
     printf("%d\n", arr[2]);
-    printf("%d\n", arr[3]); // it will not print 1 but 100
+    printf("%d\n", arr[3]);     // it will not print 1 but 100
     printf("%0.2f\n", b[1]);
     printf("%c\n", ch[3]);
     
--- Arrays/user_Input.c
+++ Arrays/user_Input.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 int main () {
+    int a [4] ;
+    for (int i = 0; i <= 3; i++)
+    {
+        printf("Enter element no %d of array : ", i+1);
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i <= 3; i++)
+    {
+        printf("%d ", a[i]);
+    }
     
     return 0;
 }
