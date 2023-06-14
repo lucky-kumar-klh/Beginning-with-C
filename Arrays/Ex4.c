b1d0c46b7b5793e66328192298a9f4f79d249f3a
Lucky <lk5999950@gmail.com>
2023-06-15 01:32:02 +0530

    First Commit

--- Arrays/Ex4.c
+++ Arrays/Ex4.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 int main() {
-    
+    int array[7] = {1,2,3,4,5,6,7};
+    int sum1 = 0, sum2 = 0;
+    for (int i = 0; i < 7; i=i+2){   // even indices
+        sum1 = sum1 + array[i];
+    }
+    for (int i = 1; i < 7; i=i+2){   // odd indices
+        sum2 = sum2 + array[i];
+    }
+    int diff = sum1 - sum2;
+    printf("%d\n", diff);
     return 0;
 }
