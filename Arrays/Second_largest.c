6ee166c82c029e7245c31a0939807204e45fc948
Lucky <lk5999950@gmail.com>
2023-06-16 10:02:47 +0530

    First Commit

--- Arrays/Second_largest.c
+++ Arrays/Second_largest.c
@@ -19,21 +19,21 @@
 //     printf("%d\n", max2);
 //     return 0;
 // }
-//---------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------//
 
 # include <stdio.h>
 #include <limits.h>
 int main() {
-    int array[8] = {1,2,11,4,5,6,19,8};
+    int array[7] = {12,6,8,44,37,23,11};
     int max2, max1 = INT_MIN;
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 7; i++)
     {
         if ( max1 < array[i] ){
-            max2 = max1;
-            max1 = array[i];
+            max2 = max1;        // max 2 is previous max 1.
+            max1 = array[i];   // max 1 is new max 1.
         }
-        else if ( max2 < array[i] ){
-            max2 = array[i];
+        else if ( max2 < array[i] && max1 != array[i] ){   // This condition will only run when above if condition will not work.
+            max2 = array[i];            
         }
     }
     printf("Second largest element is %d\n", max2);
