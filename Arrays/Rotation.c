8ae29ca6a5520c30dfde0df53f3539ab5eeba46d
Lucky <lk5999950@gmail.com>
2023-06-19 00:18:24 +0530

    First Commit

--- Arrays/Rotation.c
+++ Arrays/Rotation.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+void rotate(int arr[], int p, int q){
+    for (int i=p, j=q; i<=j; i++,j--){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return;
+}
 int main() {
+    int arr[7] = {1,2,3,4,5,6,7};
+    rotate(arr, 2, 5);
+    for (int i = 0; i < 7; i++){
+        printf("%d ", arr[i]);
+    }
     return 0;
 }
