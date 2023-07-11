6149ca7cec9b34f2a835dac65e6914aba0353c36
Lucky <lk5999950@gmail.com>
2023-07-11 14:52:50 +0530

    First Commit

--- Arrays/Reverse_usingloop.c
+++ Arrays/Reverse_usingloop.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-void reverse(int array[]){
-    int i=0, j=5;
-    while ( i<=j )    // for( int i=0, j=5; i<j; i++,j--)
+void reverse(int array[], int a, int b){
+    int i, j;
+    for (int i = a, j = b; i < j; i++, j--)
     {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
-        i++;
-        j--;
-    }   
+    }
+      
     return;
 }
 int main() {
     int array[6] = {1,2,3,4,5,6};
-    reverse(array);
+    reverse(array, 2, 5);
     for (int i = 0; i < 6; i++)
     {
         printf("%d ", array[i]);
