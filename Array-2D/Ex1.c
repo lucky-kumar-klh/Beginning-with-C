c39a98d6b7f4bec47a00f6a2e412a5a2436beb52
Luckyx100 <technicallucky861@gmail.com>
2023-07-15 19:43:15 +0530

    First Commit

--- Array-2D/Ex1.c
+++ Array-2D/Ex1.c
@@ -7,11 +7,11 @@ int main() {
     int arr[2][c];
     printf("Enter Students roll no : \n");
     for ( i = 0; i < 2; i++){
-        scanf("%d ", arr[i][0]);
+        scanf("%d ", &arr[i][0]);
     }
     printf("Enter marks of students : \n");
     for ( i = 0; i < 2; i++){
-        scanf("%d ", arr[i][1]);
+        scanf("%d ", &arr[i][1]);
     }
     for ( i = 0; i < 2; i++){
         for ( j = 0; j < c; j++){
