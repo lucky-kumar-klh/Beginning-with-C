2c74885688e44cb4048009ecc7d8bd4f09f2f86f
Luckyx100 <technicallucky861@gmail.com>
2023-07-16 13:54:42 +0530

    First Commit

--- Array-2D/adding_matrix.c
+++ Array-2D/adding_matrix.c
@@ -2,7 +2,7 @@
 int main() {
     int arr[2][2] = {1,2,3,4};
     int brr[2][2] = {1,2,3,4};
-    int result[2][2];
+    int result[2][2]; //hi
     for (int i = 0; i < 2; i++){
         for (int j = 0; j < 2; j++){
             result[i][j] = arr[i][j] + brr[i][j]; 
