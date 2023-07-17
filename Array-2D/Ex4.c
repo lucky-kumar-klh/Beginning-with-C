c058e9d9c72169d95ad29b2fc5c7ca816aaf1054
Luckyx100 <technicallucky861@gmail.com>
2023-07-17 22:27:31 +0530

    First Commit

--- Array-2D/Ex4.c
+++ Array-2D/Ex4.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main(){
+    int sum = 0;
     int arr[3][3] = {{2,3,1},{5,7,0},{9,4,8}};
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            
+        }
+        
+    }
     
     return 0;
 }
