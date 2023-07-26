04154e9c4ea0dab66277b7072e322f298ec305c5
Luckyx100 <technicallucky861@gmail.com>
2023-07-26 11:04:30 +0530

    First Commit

--- Array-2D/Ex5.c
+++ Array-2D/Ex5.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
 int main(){
+    int i, j;
+    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    for ( j = 0; j < 3; j++){
+        if ( j % 2 == 0 ){
+            for ( i = 2; i >= 0; i--){
+                printf("%d  ", arr[i][j]);
+            }
+            //printf("\n");
+        }
+        else {
+            for ( i = 0; i < 3; i++){
+                printf("%d  ", arr[i][j]);
+            }
+            //printf("\n");
+        }
+    }
+    
     return 0;
 }
