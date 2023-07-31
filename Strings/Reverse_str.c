9949daac264111e6680b905a44f4ab0a51f5973a
Luckyx100 <technicallucky861@gmail.com>
2023-07-31 23:31:48 +0530

    First Commit

--- Strings/Reverse_str.c
+++ Strings/Reverse_str.c
@@ -1,4 +1,20 @@
 #include<stdio.h>
 int main(){
+    char str[100];
+    printf("Enter your string : ");
+    gets(str);
+    printf("Size of this string is : ");
+    int i = 0, number = 0;
+    while ( str[i] != '\0' ){
+        number++;
+        i++;
+    }
+    int size = number;
+    printf("%d\n", size);
+    printf("Reverse of this string is : ");
+    for (int i = size; i >= 0; i--){
+        printf("%c", str[i]);
+    }
+    
     return 0;
 }
--- xyz.c
+++ xyz.c
@@ -1,16 +1,4 @@
 #include <stdio.h>
-int main(){
-	char str[] = "ababd";
-    int a = 0;
-	for (int i = 0, j = 4; i < j; i++, j--){
-		if ( str[i] == str[j] ) a = 1;
-		else{
-			a = 0;
-			break;
-		}
-	}
-	if ( a == 1 ) printf("Yes\n");
-	else printf("No\n");
-	
+int main(){	
 	return 0;
 } 
