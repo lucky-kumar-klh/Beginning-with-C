3ccd377154efee5bca759fa4b64eef7e38bcb736
Luckyx100 <technicallucky861@gmail.com>
2023-08-09 11:21:38 +0530

    First Commit

--- xyz.c
+++ xyz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(){	
-	int a = 0;
-	//a++*3;
-	a = a++*3;
-	printf("%d\n",a);
+	int i, j, k = 0;
+	//i = j = k = 0;
+	k = ++i*++j;
+	printf("%d\n",i+j+k);
 	return 0;
 } 
