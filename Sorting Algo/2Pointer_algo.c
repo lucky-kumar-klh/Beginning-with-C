9e0904b0806662df65002f03054a1eb19e416a3b
Luckyx100 <technicallucky861@gmail.com>
2023-08-21 11:00:16 +0530

    First Commit

--- Sorting Algo/2Pointer_algo.c
+++ Sorting Algo/2Pointer_algo.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
-    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-    int i = 0, j = 9;
+    int n;
+    int arr[n];
+    int i = 0, j = n-1;
     // for (int i = 0, j = 9; i < j; i++, j--){
     //     if ( arr[i] + arr[j] == 12 ){
     //         printf("Yeah !\n");
