da9b6888b4a2aaa12297f3fe0607b69a1164ac17
Luckyx100 <technicallucky861@gmail.com>
2023-10-03 14:29:49 +0530

    commit

--- Loop_Concepts/For_Loop/Pascal_Triangle.c
+++ Loop_Concepts/For_Loop/Pascal_Triangle.c
@@ -5,15 +5,16 @@ int main(){
     scanf("%d", &n);
     for (int i = 0; i <= n; i++)
     {
+        // for spaces
         for (int k = 0; k <= n-i; k++)
         {
-            printf(" ");
+            printf("  ");
         }
-        
+        // for numbers
         int first = 1;
         for (int j = 0; j <= i; j++)
         {
-            printf("%d ", first);
+            printf(" %2d ", first);
             first = first * (i-j)/(j+1);   // nC(r+1) = nCr * (n-r)/(r+1) 
         }
         printf("\n");
