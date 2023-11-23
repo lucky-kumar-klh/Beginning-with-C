8ee9ed8dc3780a14864139e435f60107d3eb867b
Luckyx100 <technicallucky861@gmail.com>
2023-11-23 20:42:39 +0530

    commit

--- xyz.c
+++ xyz.c
@@ -1,32 +1,5 @@
 #include <stdio.h>
 int main(){
-	int balance = 0, withdraw, deposit, choice, withdrawAmt, depositAmt;
-	printf("Enter initial balance : ");
-	scanf("%d", &balance);
-	printf("Select any two options :\n1. Withdraw\n2. Deposit\nEnter your choice : ");
-	scanf("%d", &choice);
-	switch (choice){
-	case 1:
-		printf("Enter Withdraw amt : ");
-		scanf("%d", &withdrawAmt);
-		if ( withdrawAmt > balance ) {
-			printf("Insuffiecient Balance");
-			break;
-		}
-		else {
-			balance -= withdrawAmt;
-			printf("New Balance = %d", balance);
-		}
-		break;
 	
-	case 2:
-		printf("Enter Deposit amt : ");
-		scanf("%d", &depositAmt);
-		balance += depositAmt;
-		printf("New Balance = %d", balance);
-		break;
-	default:
-		printf("BSDK");
-	}
-	
+	return 0;
 }
