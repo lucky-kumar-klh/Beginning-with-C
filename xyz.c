ef9cc04337a2c44d17e461839870e11e0b88397d
Luckyx100 <lk5999950@gmail.com>
2023-12-04 00:08:08 +0530

    commit

--- xyz.c
+++ xyz.c
@@ -1,41 +1,74 @@
 #include <stdio.h>
-// Binary search function.
-int binary_search(int arr[],int lower,int upper,int num)
-{
-    int mid = (lower+upper)/2;
-    if(num==arr[mid]) return mid;
-    else if(num<arr[mid]) upper = mid - 1;
-    else lower = mid + 1;
+#include <stdlib.h>
 
-    // If lower > upper it means element doesnot exist in array. So return -1.
-    if(lower>upper) return -1; 
-    // Else call the binary search function with new lower and upper values.
-    else binary_search(arr,lower,upper,num);
+struct node{	
+	int data;
+	struct node *link;
+};
+struct node *head = NULL;
+void create(){
+	struct node *t, *temp;
+	int choice, x;
+	do {
+		// allocate memory
+		t = (struct node *)malloc(sizeof(struct node));
+		t -> link = NULL;
+		printf("Enter data : ");
+		scanf("%d", &x);
+		t -> data = x;
+		if (head == NULL){
+			head = t;
+			temp = head;
+		}
+		else {
+			temp -> link = t;
+			temp = temp -> link;
+		}
+		printf("Press 1 to add data, 0 to exit : ");
+		scanf("%d", &choice);
+	} 
+	while (choice != 0);
+}
+void display(){
+	struct node *temp;
+	temp = head;
+	printf("\nElements => ");
+	while (temp != NULL){
+		printf("%d ", temp -> data);
+		temp = temp -> link;
+	}
+	printf("\n");
+}
+void sum (){
+  struct node *temp = head;
+  float sum = 0, count = 0;
+  while (temp != NULL){
+    sum += temp->data;
+    count++;
+    temp = temp->link;
+  }
+  float avg = sum/count;  
+  printf("\nSum = %.0f, Total Elements = %.0f, Avg = %.2f\n", sum, count, avg);
+}
+void max_min (){
+  struct node *temp = head;
+  int max = -99999, min = 999999;
+  while (temp != NULL){
+    if (temp->data > max){
+      max = temp->data;
+    }
+    if (temp->data < min){
+      min = temp->data;
+    }
+    temp = temp->link;
+  } 
+  printf("\nMaximum element = %d\nMinimum element = %d\n", max, min);
 }
 
-int main()
-{
-    int size;
-    // Step-1 Scanning the size of the array.
-    printf("Please enter the size of the array: ");
-    scanf("%d",&size);
-    int array[size];
-    // Step-2 Scanning the array.
-    printf("Please enter the array: ");
-    for(int i=0;i<size;i++)
-    {
-        scanf("%d",&array[i]);
-    }
-    // Step-3 Scanning the number to search.
-    int num;
-    printf("Please enter the number to search: ");
-    scanf("%d",&num);
-    // Step-4 Declaring the initial lower and upper values.
-    int lower=0,upper=size-1;
-    // Step-5 Using a flag variable to store the position of requires number.
-    int flag = binary_search(array,lower,upper,num);
-    // Step-6 Printing the postion stores in flag variable.
-    if(flag == -1) printf("The element does not exist in array.");
-    else printf("The element is present in %d position.",flag);
-    return 0;
+int main(){
+  create();
+	display();
+	sum();
+	max_min();
+  return 0;
 }
--- xyz.cpp
+++ xyz.cpp
@@ -1,30 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-void sum (){
-  struct node *temp = head;
-  float sum = 0, count = 0;
-  while (temp != NULL){
-    sum += temp->data;
-    count++;
-    temp = temp->link;
-  }
-  float avg = sum/count;  
-  printf("Sum = %.0f, count = %.0f, Avg = %.2f", sum, count, avg);
-}
-void max_min (){
-  struct node *temp = head;
-  int max = -99999, min = 999999;
-  while (temp != NULL){
-    if (temp->data > max){
-      max = temp->data;
-    }
-    if (temp->data < min){
-      min = temp->data;
-    }
-    temp = temp->link;
-  } 
-  printf("Maximum element = %d\nMinimum element = %d", max, min);
-}
+
 int main(){
   
   return 0;
