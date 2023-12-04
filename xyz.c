0303dae5b2797d954c2b775737a83c9b02d56236
Luckyx100 <lk5999950@gmail.com>
2023-12-05 00:44:11 +0530

    commit

--- xyz.c
+++ xyz.c
@@ -1,74 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct node{	
+struct node{
 	int data;
-	struct node *link;
+	struct node *next;
 };
 struct node *head = NULL;
-void create(){
-	struct node *t, *temp;
-	int choice, x;
+void insertNode(struct node **s, int value){ //
+	struct node *new, *temp;
+	new = (struct node *)malloc(12);
+	new -> data = value;
+	new -> new = NULL;
+	if (*s == NULL){ // emply list
+		*s = new;
+	}
+	else { // existing list
+		temp = *s // temp is pointing to node whose address is in start
+		while ( temp -> next != NULL ){ // make temp node move to second last node
+			temp = temp -> next;
+		}
+		temp -> next = new;
+	}
+}
+void userInsert(){
+	struct node *temp, *new;
+	int inp, choice;
 	do {
-		// allocate memory
-		t = (struct node *)malloc(sizeof(struct node));
-		t -> link = NULL;
+		new = (struct node *)malloc(12);
 		printf("Enter data : ");
-		scanf("%d", &x);
-		t -> data = x;
-		if (head == NULL){
-			head = t;
-			temp = head;
+		scanf("%d", &inp);
+		new -> data = inp;
+		new -> next = NULL;
+		if (head == NULL){  // empty list (only head is present initially)
+			head = new;  // assigning address of new node to starting/head node
+			temp = head; // temp just track the 1st node (that is after head node)
 		}
-		else {
-			temp -> link = t;
-			temp = temp -> link;
+		else { // already existing nodes 
+			temp -> next = new;  // previos node (i,e,. temp) storing address of new (new node)
+			temp = temp -> next; // pointing to last node (i,e. new)
 		}
-		printf("Press 1 to add data, 0 to exit : ");
+		printf("Press 1 to continue, 0 to stop : ");
 		scanf("%d", &choice);
 	} 
 	while (choice != 0);
 }
-void display(){
-	struct node *temp;
-	temp = head;
-	printf("\nElements => ");
-	while (temp != NULL){
-		printf("%d ", temp -> data);
-		temp = temp -> link;
-	}
-	printf("\n");
-}
-void sum (){
-  struct node *temp = head;
-  float sum = 0, count = 0;
-  while (temp != NULL){
-    sum += temp->data;
-    count++;
-    temp = temp->link;
-  }
-  float avg = sum/count;  
-  printf("\nSum = %.0f, Total Elements = %.0f, Avg = %.2f\n", sum, count, avg);
-}
-void max_min (){
-  struct node *temp = head;
-  int max = -99999, min = 999999;
-  while (temp != NULL){
-    if (temp->data > max){
-      max = temp->data;
-    }
-    if (temp->data < min){
-      min = temp->data;
-    }
-    temp = temp->link;
-  } 
-  printf("\nMaximum element = %d\nMinimum element = %d\n", max, min);
-}
-
 int main(){
-  create();
-	display();
-	sum();
-	max_min();
-  return 0;
+	struct node *start = NULL;
+	insertNode(&start, 24);
+	userInsert();
+	return 0;
 }
