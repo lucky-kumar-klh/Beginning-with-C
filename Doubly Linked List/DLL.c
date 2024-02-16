ad0adb36eb01e5216b09687461be4f3031bc7b76
Luckyx100 <technicallucky861@gmail.com>
2024-02-16 19:17:51 +0530

    commit

---
+++ Doubly Linked List/DLL.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+
+    int data;
+    Node* prev;
+    Node* next;
+}Node;
+
+Node* head -> next = NULL;
+head -> prev = NULL;
+
+void create
+
+int main(){
+
+    
+    return 0;
+}
