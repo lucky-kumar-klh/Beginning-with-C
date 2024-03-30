c79688b517c38cf3f1025dcf2c3f1d01424037df
lucky-kumar-klh <2310080034@klh.edu.in>
2024-03-30 17:07:51 +0530

    commit

--- Linked List/insertNode.c
+++ Linked List/insertNode.c
@@ -9,17 +9,17 @@ struct Node{
 void insertData(struct Node **head, int value){
   struct Node *new, *temp;
   // allocate new node address
-  new = (struct Node *)malloc(sizeof(struct Node)); 
+  new = (struct Node *)malloc(sizeof(struct Node));
   // assign value to this new node
   new -> data = value;
   // since this is the last created node, it's link part has NULL
   new -> link = NULL;
-  // check to which list this new node will get linked with 
+  // check to which list this new node will get linked with
   if (*head == NULL){  // empty list
     // store created node's address in head
     *head = new;
   }
-  else { // existing list 
+  else { // existing list
     // use any temp struct to point to first node
     temp = *head;
     while (temp -> link != NULL){ // move temp to second last node to link to the last node
--- xyz.cpp
+++ xyz.cpp
@@ -1,88 +1,88 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-class Node{
+class Node {
 
-  public:
-    int data;
-    Node* link;
-  
-  Node(int data){
-    this -> data = data;
-    this -> link = NULL; 
+public:
+  int data;
+  Node *link;
+
+  Node(int data) {
+    this->data = data;
+    this->link = NULL;
   }
 };
 
-void insertAtEnd(Node* &tail, int d){  // tail already given
-  Node* temp = new Node(d);
-  tail -> link = temp;
-  tail = temp; 
+void insertAtEnd(Node *&tail, int d) { // tail already given
+  Node *temp = new Node(d);
+  tail->link = temp;
+  tail = temp;
 }
 
-void insertAtHead(Node* &head, int d){
-  Node* temp = new Node(d);
-  temp -> link = head;
+void insertAtHead(Node *&head, int d) {
+  Node *temp = new Node(d);
+  temp->link = head;
   head = temp;
 }
 
-void insertAtPosition(Node* &tail, Node* &head, int position, int d){
+void insertAtPosition(Node *&tail, Node *&head, int position, int d) {
   // 1st Node insertion
-  if (position == 1){
+  if (position == 1) {
     insertAtHead(head, d);
     return;
   }
-  Node* temp = head;
+  Node *temp = head;
   int count = 1;
 
-  while (count < position-1){
-    temp = temp -> link;
+  while (count < position - 1) {
+    temp = temp->link;
     count++;
   }
 
   // Check for Last Node
-  if (temp != NULL){
+  if (temp != NULL) {
     insertAtEnd(tail, d);
     return;
   }
 
-  Node* t = new Node(d);
-  t -> link = temp -> link;
-  temp -> link = t;
+  Node *t = new Node(d);
+  t->link = temp->link;
+  temp->link = t;
 }
 
-void printList(Node* &head){
-  if (head == NULL){
+void printList(Node *&head) {
+  if (head == NULL) {
     cout << "Empty List !" << endl;
     return;
   }
-  Node* temp = head;
-  while (temp != NULL){
-    cout << temp -> data << " ";
-    temp = temp -> link;
+  Node *temp = head;
+  while (temp != NULL) {
+    cout << temp->data << " ";
+    temp = temp->link;
   }
   cout << endl;
 }
 
-int main(){
-  
-  Node* node1 = new Node(10);
-  Node* head = node1;
-  Node* tail = node1;
-  
+int main() {
+
+  Node *node1 = new Node(10);
+  Node *head = node1;
+  Node *tail = node1;
+
   insertAtEnd(tail, 3);
-  
+
   insertAtHead(head, 5);
-  
+
   printList(tail);
-  
+
   insertAtEnd(tail, 12);
-  
+
   insertAtPosition(tail, head, 2, 20);
-  
+
   printList(head);
 
-  cout << "Head : " << head -> data << endl;
-  cout << "Tail : " << tail -> data << endl;
+  cout << "Head : " << head->data << endl;
+  cout << "Tail : " << tail->data << endl;
 
   return 0;
 }
